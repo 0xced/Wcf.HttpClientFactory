a8b76cb7d38a97b505d91bcc19a431a8742e82aa
Cédric Luthi <cedric.luthi@gmail.com>
2023-08-30 18:00:51 +0200

    DefaultBinding + DefaultEndpointAddress
    See https://github.com/dotnet/wcf/issues/5179#issuecomment-1699293014

---
+++ src/CalculatorService/HelloEndpointClient.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+// ReSharper disable once CheckNamespace
+namespace ServiceReference;
+
+public partial class CalculatorSoapClient
+{
+    public static Binding DefaultBinding { get; } = GetBindingForEndpoint(EndpointConfiguration.CalculatorSoap);
+
+    public static EndpointAddress DefaultEndpointAddress { get; } = GetEndpointAddress(EndpointConfiguration.CalculatorSoap);
+}
---
+++ src/HelloService/HelloEndpointClient.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+// ReSharper disable once CheckNamespace
+namespace ServiceReference;
+
+public partial class HelloEndpointClient
+{
+    public static Binding DefaultBinding { get; } = GetDefaultBinding();
+
+    public static EndpointAddress DefaultEndpointAddress { get; } = GetDefaultEndpointAddress();
+}
