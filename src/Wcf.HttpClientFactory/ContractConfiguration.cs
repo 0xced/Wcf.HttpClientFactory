8069ff206520ab981a615821c269f99daf63d319
Cédric Luthi <cedric.luthi@gmail.com>
2023-12-16 17:08:24 +0100

    Merge ContractConfiguration and ContractConfiguration<TContract> classes
    They don't need to be separate classe.

--- src/Wcf.HttpClientFactory/ContractConfiguration.cs
+++ src/Wcf.HttpClientFactory/ContractConfiguration.cs
@@ -1,74 +1,10 @@
 namespace Wcf.HttpClientFactory;
 
-public abstract class ContractConfiguration
-{
-    private readonly ContractDescription _contractDescription;
-    private ServiceEndpoint? _serviceEndpoint;
-
-    protected Type ClientType { get; }
-
-    protected ContractConfiguration(ContractDescription contractDescription)
-    {
-        _contractDescription = contractDescription;
-        ClientType = _contractDescription.GetClientType();
-    }
-
-    internal ServiceEndpoint GetServiceEndpoint(string httpClientName, HttpMessageHandlerBehavior httpMessageHandlerBehavior)
-    {
-        // Make sure that the ServiceEndpoint is the exact same instance for ClientBase caching to work properly, see https://github.com/dotnet/wcf/issues/5353
-        if (_serviceEndpoint == null)
-        {
-            var binding = GetBinding();
-            var endpointAddress = GetEndpointAddress();
-            _serviceEndpoint = new HttpServiceEndpoint(httpClientName, _contractDescription, binding, endpointAddress);
-            _serviceEndpoint.EndpointBehaviors.Add(httpMessageHandlerBehavior);
-        }
-        return _serviceEndpoint;
-    }
-
-    protected virtual Binding GetBinding()
-    {
-        var getDefaultBinding = ClientType.GetMethod("GetDefaultBinding", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-        if (getDefaultBinding != null)
-            return (Binding)(getDefaultBinding.Invoke(null, Array.Empty<object>()) ?? throw new InvalidOperationException($"{ClientType.FullName}.{getDefaultBinding.Name} returned null"));
-
-        var getBindingForEndpoint = ClientType.GetMethod("GetBindingForEndpoint", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-        if (getBindingForEndpoint != null)
-            return (Binding)(getBindingForEndpoint.Invoke(null, new object[] { 0 }) ?? throw new InvalidOperationException($"{ClientType.FullName}.{getBindingForEndpoint.Name} returned null"));
-
-        throw new MissingMethodException(MissingMethodMessage(ClientType, "GetBindingForEndpoint"));
-    }
-
-    protected virtual EndpointAddress GetEndpointAddress()
-    {
-        var getDefaultEndpointAddress = ClientType.GetMethod("GetDefaultEndpointAddress", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-        if (getDefaultEndpointAddress != null)
-            return (EndpointAddress)(getDefaultEndpointAddress.Invoke(null, Array.Empty<object>()) ?? throw new InvalidOperationException($"{ClientType.FullName}.{getDefaultEndpointAddress.Name} returned null"));
-
-        var getEndpointAddress = ClientType.GetMethod("GetEndpointAddress", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-        if (getEndpointAddress != null)
-            return (EndpointAddress)(getEndpointAddress.Invoke(null, new object[] { 0 }) ?? throw new InvalidOperationException($"{ClientType.FullName}.{getEndpointAddress.Name} returned null"));
-
-        throw new MissingMethodException(MissingMethodMessage(ClientType, "GetEndpointAddress"));
-    }
-
-    protected virtual void ConfigureEndpoint(ServiceEndpoint endpoint, ClientCredentials clientCredentials)
-    {
-    }
-
-    private static string MissingMethodMessage(Type clientType, string missingMethodName)
-    {
-        return $"The method {clientType.FullName}.{missingMethodName} was not found. " +
-               $"Was {clientType.FullName} generated with the https://www.nuget.org/packages/dotnet-svcutil tool?";
-    }
-}
-
-public class ContractConfiguration<TContract> : ContractConfiguration
+public class ContractConfiguration<TContract>
     where TContract : class
 {
-    [SuppressMessage("ReSharper", "StaticMemberInGenericType", Justification = "One value per closed type is what is needed")]
+    [SuppressMessage("ReSharper", "StaticMemberInGenericType", Justification = "One value per closed type is what is needed as it's actually constructed from TContract")]
     private static ContractDescription? _contractDescription;
-
     internal static ContractDescription ContractDescription
     {
         get
@@ -78,29 +14,79 @@ public class ContractConfiguration<TContract> : ContractConfiguration
         }
     }
 
-    private readonly ConstructorInfo _clientConstructor;
+    private readonly Type _clientType = ContractDescription.GetClientType();
 
-    [SuppressMessage("ReSharper", "MemberCanBeProtected.Global", Justification = "It needs to be public in order to be instantiated by the dependency injection container")]
-    public ContractConfiguration() : base(ContractDescription)
+    private ConstructorInfo? _clientConstructor;
+    private ConstructorInfo ClientConstructor
     {
-        var clientConstructor = ClientType.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, new[] { typeof(ServiceEndpoint) });
-        if (clientConstructor == null)
+        get
         {
-            var namespaceLines = ClientType.Namespace == null ? "" : $"{Environment.NewLine}namespace {ClientType.Namespace};{Environment.NewLine}";
-            var message = $$"""
-                            The {{ClientType.Name}} class is missing a constructor taking a ServiceEndpoint parameter. Please add the following code in your project:
-                            {{namespaceLines}}
-                            {{(ClientType.IsPublic ? "public" : "internal")}} partial class {{ClientType.Name}}
-                            {
-                                public {{ClientType.Name}}(System.ServiceModel.Description.ServiceEndpoint endpoint) : base(endpoint)
-                                {
-                                }
-                            }
+            if (_clientConstructor == null)
+            {
+                _clientConstructor = _clientType.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, new[] { typeof(ServiceEndpoint) });
+                if (_clientConstructor == null)
+                {
+                    var namespaceLines = _clientType.Namespace == null ? "" : $"{Environment.NewLine}namespace {_clientType.Namespace};{Environment.NewLine}";
+                    var message = $$"""
+                                    The {{_clientType.Name}} class is missing a constructor taking a ServiceEndpoint parameter. Please add the following code in your project:
+                                    {{namespaceLines}}
+                                    {{(_clientType.IsPublic ? "public" : "internal")}} partial class {{_clientType.Name}}
+                                    {
+                                        public {{_clientType.Name}}(System.ServiceModel.Description.ServiceEndpoint endpoint) : base(endpoint)
+                                        {
+                                        }
+                                    }
 
-                            """.ReplaceLineEndings();
-            throw new MissingMemberException(message);
+                                    """.ReplaceLineEndings();
+                    throw new MissingMemberException(message);
+                }
+            }
+            return _clientConstructor;
         }
-        _clientConstructor = clientConstructor;
+    }
+
+    protected virtual Binding GetBinding()
+    {
+        var getDefaultBinding = _clientType.GetMethod("GetDefaultBinding", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        if (getDefaultBinding != null)
+            return (Binding)(getDefaultBinding.Invoke(null, Array.Empty<object>()) ?? throw new InvalidOperationException($"{_clientType.FullName}.{getDefaultBinding.Name} returned null"));
+
+        var getBindingForEndpoint = _clientType.GetMethod("GetBindingForEndpoint", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        if (getBindingForEndpoint != null)
+            return (Binding)(getBindingForEndpoint.Invoke(null, new object[] { 0 }) ?? throw new InvalidOperationException($"{_clientType.FullName}.{getBindingForEndpoint.Name} returned null"));
+
+        throw MissingMethodException("GetBindingForEndpoint");
+    }
+
+    protected virtual EndpointAddress GetEndpointAddress()
+    {
+        var getDefaultEndpointAddress = _clientType.GetMethod("GetDefaultEndpointAddress", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        if (getDefaultEndpointAddress != null)
+            return (EndpointAddress)(getDefaultEndpointAddress.Invoke(null, Array.Empty<object>()) ?? throw new InvalidOperationException($"{_clientType.FullName}.{getDefaultEndpointAddress.Name} returned null"));
+
+        var getEndpointAddress = _clientType.GetMethod("GetEndpointAddress", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        if (getEndpointAddress != null)
+            return (EndpointAddress)(getEndpointAddress.Invoke(null, new object[] { 0 }) ?? throw new InvalidOperationException($"{_clientType.FullName}.{getEndpointAddress.Name} returned null"));
+
+        throw MissingMethodException("GetEndpointAddress");
+    }
+
+    protected virtual void ConfigureEndpoint(ServiceEndpoint endpoint, ClientCredentials clientCredentials)
+    {
+    }
+
+    private ServiceEndpoint? _serviceEndpoint;
+    internal ServiceEndpoint GetServiceEndpoint(string httpClientName, HttpMessageHandlerBehavior httpMessageHandlerBehavior)
+    {
+        // Make sure that the ServiceEndpoint is the exact same instance for ClientBase caching to work properly, see https://github.com/dotnet/wcf/issues/5353
+        if (_serviceEndpoint == null)
+        {
+            var binding = GetBinding();
+            var endpointAddress = GetEndpointAddress();
+            _serviceEndpoint = new HttpServiceEndpoint(httpClientName, ContractDescription, binding, endpointAddress);
+            _serviceEndpoint.EndpointBehaviors.Add(httpMessageHandlerBehavior);
+        }
+        return _serviceEndpoint;
     }
 
     internal ChannelFactory<TContract> CreateChannelFactory(ServiceEndpoint serviceEndpoint)
@@ -112,11 +98,18 @@ public class ContractConfiguration<TContract> : ContractConfiguration
 
     internal ClientBase<TContract> CreateClient(ServiceEndpoint serviceEndpoint)
     {
-        var client = (ClientBase<TContract>)_clientConstructor.Invoke(new object[] { serviceEndpoint });
+        var client = (ClientBase<TContract>)ClientConstructor.Invoke(new object[] { serviceEndpoint });
         if (client.ChannelFactory.State == CommunicationState.Created)
         {
             ConfigureEndpoint(client.Endpoint, client.ClientCredentials);
         }
         return client;
+    }
+
+    private Exception MissingMethodException(string missingMethodName)
+    {
+        var message = $"The method {_clientType.FullName}.{missingMethodName} was not found. " +
+                      $"Was {_clientType.Name} generated with the https://www.nuget.org/packages/dotnet-svcutil tool?";
+        return new MissingMethodException(message);
     }
 }
